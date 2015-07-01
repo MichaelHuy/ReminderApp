830a33c324de6f17b95a6ed46dd946379132eaa7
MichaelHuy <lehuydit@gmail.com>
2015-07-01 07:15:47 +0700

    update for my owwn

--- server/app.js
+++ server/app.js
@@ -24,29 +24,6 @@ var host = (process.env.VCAP_APP_HOST || 'localhost');
 app.use('/', require('./routes/index'));
 app.set('port', port || 3000);
 
-
-// app.get('/', function(req, res){
-// 	res.sendfile('public/index.html');
-// });
-
-// // This responds a DELETE request for the /del_user page.
-// app.delete('/del_user', function (req, res) {
-//    console.log("Got a DELETE request for /del_user");
-//    res.send('Hello DELETE');
-// })
-
-// // This responds a GET request for the /list_user page.
-// app.get('/list_user', function (req, res) {
-//    console.log("Got a GET request for /list_user");
-//    res.send('Page Listing');
-// })
-
-// // This responds a GET request for abcd, abxcd, ab123cd, and so on
-// app.get('/ab*cd', function(req, res) {   
-//    console.log("Got a GET request for /ab*cd");
-//    res.send('Page Pattern Match');
-// })
-
 var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + server.address().port);
 });
--- server/logic/twilio/triggerCall.js
+++ server/logic/twilio/triggerCall.js
@@ -6,7 +6,7 @@ var call = {
       to: reminder.user.phone,
       from: '+12014312641',
       //url: 'http://20585314.ngrok.com/call/twiml/' + reminder.user._id + '/' + reminder._id
-      url: 'http://rem.mybluemix.net/call/twiml/' + reminder.user._id + '/' + reminder._id      
+      url: 'http://remidermiddleware.mybluemix.net/call/twiml/' + reminder.user._id + '/' + reminder._id      
     }, function(error, response) {
       console.log(error || response);
     });
